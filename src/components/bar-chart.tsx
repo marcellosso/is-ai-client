ce461059a16e16c67b0730b864712528d0169e4f
Marcel Losso <marcel@finsera.com>
2022-09-28 21:44:13 -0300

    Added barchart component

--- src/components/bar-chart.tsx
+++ src/components/bar-chart.tsx
@@ -4,14 +4,14 @@ const BarChart = () => {
   return (
     <div className="h-44 flex justify-center items-end">
       <div
-        className="w-10 flex items-center justify-center mr-2 bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-md"
+        className="w-10 flex items-center justify-center mr-2 bg-blue-600 text-xs font-medium text-detail text-center p-0.5 leading-none rounded-md"
         style={{ height: '55%' }}
       >
         {' '}
         55%
       </div>
       <div
-        className="w-10 flex items-center justify-center bg-red-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-md"
+        className="w-10 flex items-center justify-center bg-red-600 text-xs font-medium text-detail text-center p-0.5 leading-none rounded-md"
         style={{ height: '45%' }}
       >
         {' '}
