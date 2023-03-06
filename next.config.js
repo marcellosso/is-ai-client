1f9eb4ceb40101470c442d903cdba962f3207381
Marcel Losso <marcel@finsera.com>
2023-03-06 16:11:52 -0300

    added crossorigin anonymous

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  crossOrigin: 'anonymous',
   images: {
     domains: [
       process.env.NEXT_PUBLIC_IMAGE_DOMAIN_LOCALHOST || '',
