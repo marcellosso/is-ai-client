a1662ee3d33f349ac2566ff415cdbff3af7d28f8
Marcel Losso <marcel@finsera.com>
2023-03-10 13:14:50 -0300

    Added images assets and seo fix

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,8 +1,7 @@
 import { DefaultSeoProps } from 'next-seo';
 
 const config: DefaultSeoProps = {
-  title:
-    'AI or Human: A game to test your abilities to determine wether the image was created by an Artifical Inteligence or by a Human.',
+  title: 'AI or Human: Test your abilities,  Artifical Inteligence or Human?',
   description:
     'AI or Human is a game where the player has to choose wether the provided image was created by an Artifical Inteligence or by a Human.',
   openGraph: {
